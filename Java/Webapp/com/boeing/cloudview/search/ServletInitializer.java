62c980a4a2af60df15376c47109db5bd9b251fcc
SUMAN KUMAR GHOSH <ghosh3337@gmail.com>
2022-12-12 13:27:45 -0800

    restructured the java class files for custom webapp

--- Java/Webapp/com/boeing/cloudview/search/ServletInitializer.java
+++ Java/Webapp/com/boeing/cloudview/search/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.exalead.boeing;
+package com.boeing.cloudview.search;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(DemoApplication.class);
+		return application.sources(CloudViewSearchApplication.class);
 	}
 
 }
