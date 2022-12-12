715133578d9a64270b4c6e95442d6cc898703ac3
SUMAN KUMAR GHOSH <91658789+Suman1921@users.noreply.github.com>
2022-12-11 23:02:00 -0800

    Update DemoApplication.java

--- Java/Webapp/com/exalead/boeing/DemoApplication.java
+++ Java/Webapp/com/exalead/boeing/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.exalead.boeing;
+package com.boeing.cloudview.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class CloudViewSearchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(CloudViewSearchApplication.class, args);
 	}
 
 }
